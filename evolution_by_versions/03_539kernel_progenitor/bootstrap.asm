aa67677054579d8d7f159c19373ac5b6396ddebf
noriyotcp <noriyo.akita@gmail.com>
2022-12-25 13:34:42 +0900

    3.2 The Basic Code of The Progenitor
    Define lables for sectors
    
    The first one, as it is obvious from its name, indicates the number of sectors that we would like our bootloader to load from the disk, the current value is 15d, which means 7.5KB from the disk will be loaded to the memory, if kernel's binary size becomes larger than 7.5KB we can simply modify the value of this label to increase the number of sectors to load.
    
    The second label indicates the sector's number that we are going to load now, as you know, sector 1 of the disk contains the bootloader (if sector numbering starts from 1), and based on our arrangement in Makefile of 539kernel, the code of the kernel will be there starting from sector 2 of the disk, therefore, the initial value of the label curr_sector_to_load is 2.
    
    Load an each sector

--- evolution_by_versions/03_539kernel_progenitor/bootstrap.asm
+++ evolution_by_versions/03_539kernel_progenitor/bootstrap.asm
@@ -12,19 +12,30 @@ start:
 	jmp 0900h:0000
 
 load_kernel_from_disk:
+	mov ax, [curr_sector_to_load]
+	sub ax, 2
+	mov bx, 512d
+	mul bx
+	mov bx, ax
+
 	mov ax, 0900h
 	mov es, ax
 
 	mov ah, 02h
 	mov al, 01h
 	mov ch, 0h
-	mov cl, 02h
+	mov cl, [curr_sector_to_load]
 	mov dh, 0h
 	mov dl, 80h
-	mov bx, 0h
 	int 13h
 
 	jc kernel_load_error
+
+	sub byte [numbers_of_sectors_to_load], 1
+	add byte [curr_sector_to_load], 1
+	cmp byte [numbers_of_sectors_to_load], 0
+
+	jne load_kernel_from_disk
 
 	ret
 
@@ -66,6 +77,8 @@ printing_finished:
 title_string		db	'The Bootloader of 539kernel.', 0
 message_string		db	'The kernel is loading...', 0
 load_error_string	db	'The kernel cannot be loaded', 0
+number_of_sectors_to_load	db	15d
+curr_sector_to_load		db	2d
 
 times 510-($-$$) db 0
 
