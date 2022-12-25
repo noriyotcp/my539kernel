f1218507e95899a5afce07ad843878b174bb4cb9
noriyotcp <noriyo.akita@gmail.com>
2022-12-26 01:22:23 +0900

    3.3.1 Remapping PICs

--- evolution_by_versions/03_539kernel_progenitor/starter.asm
+++ evolution_by_versions/03_539kernel_progenitor/starter.asm
@@ -42,6 +42,49 @@ setup_interrupts:
 
 	ret
 
+remap_pic:
+	mov al, 11h
+
+	send_init_cmd_to_pic_master:
+		out 0x20, al
+
+	send_init_cmd_to_pic_slave:
+		out 0xa0, al
+
+	make_irq_starts_from_intr_32_in_pic_master:
+		mov al, 32d
+		out 0x21, al
+
+	make_irq_starts_from_intr_40_in_pic_slave:
+		mov al, 40d
+		out 0xal, al
+
+	tell_pic_master_where_pic_slave_is_connected:
+		mov al, 04h
+		out 0x21, al
+
+	tell_pic_slave_where_pic_master_is_connected:
+		mov al, 02h
+		out 0xal, al
+
+	mov al, 01h
+
+	tell_pic_master_the_arch_is_x86:
+		out 0x21, al
+
+	tell_pic_slave_the_arch_is_x86:
+		out 0xal, al
+
+	mov al, 0h
+
+	make_pic_master_enables_all_irqs:
+		out 0x21, al
+
+	make_pic_slave_enables_all_irqs:
+		out 0xal, al
+
+	ret
+
 bits 32
 start_kernel:
 	mov eax, 10h
